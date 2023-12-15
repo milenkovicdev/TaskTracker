e678cf6dee013d6f5cb0d1566d9aa870381d9cde
Stefan Milenkovic <stefanmilenkovic@Stefans-MacBook-Pro.local>
2023-12-15 20:43:19 +0100

    Project and gitignore file initialization

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Stefan Milenkovic on 15.12.23..
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
