cbb3361bf491625a8962fd6e3db242e5a53c923c
Stefan Milenkovic <stefanmilenkovic@Stefans-MacBook-Pro.local>
2024-01-06 22:02:06 +0100

    Add no task view and some cool animation

---
+++ TaskTracker/Views/NoTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  NoTaskView.swift
+//  TaskTracker
+//
+//  Created by Stefan Milenkovic on 6.1.24..
+//
+
+import SwiftUI
+
+struct NoTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NoTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoTaskView()
+    }
+}
